e9e5b20d2258cacd5e6ce9fbc6b7d4e0fb40dafa
Kyle Mathews <mathews.kyle@gmail.com>
2020-07-31 15:02:22 -0700

    Update packages & fix plugin-manifest settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,15 +21,15 @@ module.exports = {
     `gatsby-plugin-mdx`,
     `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        name: "Dispatches from the Quarantine Church",
+        short_name: "quarantine-dispatches",
+        start_url: "/",
+        background_color: "#663399",
+        theme_color: "#663399",
+        display: "minimal-ui",
+        icon: "src/images/gatsby-icon.png",
       },
     },
     {
@@ -38,25 +38,20 @@ module.exports = {
         // You can find your read-only API token under the Settings > API tokens
         // section of your administrative area:
         apiToken: `4bebaf5f4788cdb8bf383082384a4e`,
-
         // If you are working on development/staging environment, you might want to
         // preview the latest version of records instead of the published one:
         previewMode: false,
-
         // Disable automatic reloading of content when some change occurs on DatoCMS:
         disableLiveReload: false,
-
         // Custom API base URL
         // apiUrl: "https://site-api.datocms.com/",
-
         // Setup locale fallbacks
         // In this example, if some field value is missing in Italian, fall back to English
         localeFallbacks: {
           it: ["en"],
         },
       },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
+    }, // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
